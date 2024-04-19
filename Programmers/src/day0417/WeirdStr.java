7d51e31cf3b7c3f9ba21541105fff1e9714af678
leejiyong <rosdlwl005@naver.com>
2024-04-19 22:44:53 +0900

    수정

--- Programmers/src/day0417/WeirdStr.java
+++ Programmers/src/day0417/WeirdStr.java
@@ -16,7 +16,18 @@ public class WeirdStr {
 			}
 			answer += a[i];
 		}
+		
+		String[] b = s.split(" ");
+		String answer2 = "";
+		for(int i= 0; i<b.length; i++) {
+			for(int j=0;j<b[i].length(); j++) {
+				answer2 += j%2 ==0 ? b[i].toUpperCase().charAt(j) : b[i].toLowerCase().charAt(j);
+			}
+			answer2 += " ";
+		}
 		System.out.println(answer);
+		System.out.println(answer2);
+		System.out.println(answer2.equals("TrY HeLlO WoRlD"));
 		System.out.println(answer.equals("TrY HeLlO WoRlD"));
 	}
 
