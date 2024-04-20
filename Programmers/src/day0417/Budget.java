ce1b356e9f26bf1f2ce2245097f36e312eb323df
leejiyong <rosdlwl005@naver.com>
2024-04-20 23:04:36 +0900

    4

---
+++ Programmers/src/day0417/Budget.java
@@ -0,0 +1,26 @@
+package day0417;
+
+import java.util.Arrays;
+
+public class Budget {
+	
+	public static void main(String[] args) {
+		int[] d = {1,3,2,5,4};
+		int budger = 9;
+		
+		int answer = 0;
+		int[] s = d;
+		Arrays.sort(s);
+		for(int i=0; i<s.length; i++) {
+				budger -= s[i];
+				answer++;
+			if(budger <0) {
+				answer--;
+				budger += s[i];
+				break;
+			}
+		}
+		System.out.println(answer);
+	}
+
+}
