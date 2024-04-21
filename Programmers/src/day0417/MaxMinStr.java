26612916eef6e10c9a2ef0797e46d01edd801b93
leejiyong <rosdlwl005@naver.com>
2024-04-21 20:52:25 +0900

    04

---
+++ Programmers/src/day0417/MaxMinStr.java
@@ -0,0 +1,19 @@
+package day0417;
+
+public class MaxMinStr {
+	
+	public static void main(String[] args) {
+		String t = "3141592";
+		String p = "271";
+		
+		int answer = 0;
+		for(int i=0; i<t.length()-p.length()+1; i++) {
+			String str = t.substring(i,i+p.length());
+			if(Long.parseLong(str)<=Long.parseLong(p)) {
+				answer++;
+			}
+		}
+		System.out.println(answer);
+	}
+
+}
---
+++ Programmers/src/day0417/StrPwd.java
@@ -0,0 +1,32 @@
+package day0417;
+
+public class StrPwd {
+	
+	public static void main(String[] args) {
+		String s = "a B z";
+		int n = 1;
+		String answer = "";
+		System.out.println((int)s.charAt(0));
+		for(int i=0; i<s.length(); i++) {
+			int c = s.charAt(i);
+			int t = 0;
+			if(c<=90&& c>=65) {
+				t = c + n;
+				if(t>90) {
+					t = (t-90) +64 ;
+				}
+			}else if(c<=122 &&c>=97){
+				t = c +n;
+				if(t>122) {
+					t = (t-122) + 96;
+				}
+			}else if(s.charAt(i) == ' ') {
+				answer += s.charAt(i);
+				continue;
+			}
+			answer +=(char)t;
+		}
+		System.out.println(answer);
+	}
+
+}
