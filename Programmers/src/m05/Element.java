04739fc7772c1bac71e3afaad4bf34a1f2c56861
leejiyong <rosdlwl005@naver.com>
2024-05-09 22:28:49 +0900

    05

---
+++ Programmers/src/m05/Element.java
@@ -0,0 +1,15 @@
+package m05;
+
+public class Element {
+	
+	public static void main(String[] args) {
+		int[] num_list = {2,1,6};
+		int[] answer = new int[num_list.length+1];
+		for(int i=0; i<num_list.length; i++) {
+			answer[i] = num_list[i];
+		}
+			answer[answer.length-1] = (answer[num_list.length-1]> answer[num_list.length-2]) ? (answer[num_list.length-1]- answer[num_list.length-2]):answer[num_list.length-1]*2;
+			System.out.println(answer[answer.length-1]);
+	}
+
+}
