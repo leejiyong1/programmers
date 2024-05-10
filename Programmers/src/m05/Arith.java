9930980bff51cc2ec61890735d1afd238335bdb5
leejiyong <rosdlwl005@naver.com>
2024-05-10 23:23:58 +0900

    05

---
+++ Programmers/src/m05/Arith.java
@@ -0,0 +1,19 @@
+package m05;
+
+public class Arith {
+	
+	public static void main(String[] args) {
+		int a = 3;
+		int d = 4;
+		boolean[] included = {true,false,false,true,true};
+		int answer = 0;
+		for(int i= 0;i<included.length; i++) {
+			if(included[i] == true) {
+				answer +=a;
+			}
+			a += d;			
+		}
+		System.out.println(answer);
+	}
+
+}
