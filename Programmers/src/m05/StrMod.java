43ace784dc3b050ed8a2b7346e8ef99c31a8a27f
leejiyong <rosdlwl005@naver.com>
2024-05-20 22:50:50 +0900

    05

---
+++ Programmers/src/m05/StrMod.java
@@ -0,0 +1,32 @@
+package m05;
+
+public class StrMod {
+	
+	public static void main(String[] args) {
+		String s = "banana";
+		 int answer = 0;
+	        int size = s.length();
+	        char front = s.charAt(0);
+	        int same = 0;
+	        int diff = 0;
+	        for(int i = 0 ; i < size ; i++){
+	            char ch = s.charAt(i);
+	            if(front == ch){
+	                same++;
+	            }else{
+	                diff++;
+	            }
+	            if(same == diff){
+	                answer++;
+	                same = 0;
+	                diff = 0;
+	                if(i+1 < size){
+	                    front = s.charAt(i+1);
+	                }
+	            }else if(i == size-1){
+	                answer++;
+	            }
+	        }
+	}
+
+}
